e07964f46d9f2998709ac59662b33196efd9587f
hsalmeida <hsalmeida@gmail.com>
2015-12-01 16:13:56 -0200

    tentativas de parse do html

--- js/controllers/facil.js
+++ js/controllers/facil.js
@@ -2,6 +2,15 @@ loto.controller('FacilCtrl', ['$scope', function($scope){
     $scope.arquivo = "";
     $scope.jogos = [];
     $scope.jogo = {};
+
+    $scope.predicate = 'concurso';
+    $scope.reverse = false;
+
+    $scope.order = function(predicate) {
+        $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
+        $scope.predicate = predicate;
+    };
+
 }]).directive('fileread', [function () {
     return {
         scope: {
@@ -40,6 +49,8 @@ loto.controller('FacilCtrl', ['$scope', function($scope){
                                         lista: []
                                     };
 
+                                    var regdot = new RegExp("[.]","gm");
+                                    
                                     $(trs).each(function(){
                                         var concurso = $(this).find('td:eq(0)').html();
                                         if($.isNumeric(concurso)) {
@@ -66,10 +77,16 @@ loto.controller('FacilCtrl', ['$scope', function($scope){
                                                 var bola13 = $(this).find('td:eq(14)').html();
                                                 var bola14 = $(this).find('td:eq(15)').html();
                                                 var bola15 = $(this).find('td:eq(16)').html();
+                                                var listaBolas = [];
+                                                listaBolas.push(Number(bola1), Number(bola2), Number(bola3),
+                                                    Number(bola4), Number(bola5),Number(bola6), Number(bola7),
+                                                    Number(bola8), Number(bola9), Number(bola10), Number(bola11),
+                                                    Number(bola12), Number(bola13),Number(bola14), Number(bola15));
+                                                listaBolas.sort(function(a, b){return a-b});
                                                 var arrecadacaoTotal = $(this).find('td:eq(17)').html();
                                                 var ganhadores15 = $(this).find('td:eq(18)').html();
-                                                var cidade = $(this).find('td:eq(19)').html();
-                                                var uf = $(this).find('td:eq(20)').html();
+                                                var cidade = $(this).find('td:eq(19)').html().trim();
+                                                var uf = $(this).find('td:eq(20)').html().trim();
                                                 var ganhadores14 = $(this).find('td:eq(21)').html();
                                                 var ganhadores13 = $(this).find('td:eq(22)').html();
                                                 var ganhadores12 = $(this).find('td:eq(23)').html();
@@ -86,7 +103,7 @@ loto.controller('FacilCtrl', ['$scope', function($scope){
                                                 var acumuladorEspecial = $(this).find('td:eq(32)').html();
 
                                                 var jogo = {
-                                                    concurso: concurso,
+                                                    concurso: Number(concurso),
                                                     data : data,
                                                     bola1: bola1,
                                                     bola2: bola2,
@@ -103,22 +120,23 @@ loto.controller('FacilCtrl', ['$scope', function($scope){
                                                     bola13: bola13,
                                                     bola14: bola14,
                                                     bola15: bola15,
+                                                    listaBolas : listaBolas,
                                                     arrecadacaoTotal: arrecadacaoTotal,
-                                                    ganhadores15: ganhadores15,
+                                                    ganhadores15: Number(ganhadores15),
                                                     cidades: [],
                                                     ufs: [],
-                                                    ganhadores14: ganhadores14,
-                                                    ganhadores13: ganhadores13,
-                                                    ganhadores12: ganhadores12,
-                                                    ganhadores11: ganhadores11,
-                                                    rateio15: rateio15,
-                                                    rateio14: rateio14,
-                                                    rateio13: rateio13,
-                                                    rateio12: rateio12,
-                                                    rateio11: rateio11,
-                                                    acumulado15: acumulado15,
-                                                    estimativa: estimativa,
-                                                    acumuladorEspecial: acumuladorEspecial
+                                                    ganhadores14: Number(ganhadores14),
+                                                    ganhadores13: Number(ganhadores13),
+                                                    ganhadores12: Number(ganhadores12),
+                                                    ganhadores11: Number(ganhadores11),
+                                                    rateio15: Number(rateio15.replace(regdot,'').replace(',','.')),
+                                                    rateio14: Number(rateio14.replace(regdot,'').replace(',','.')),
+                                                    rateio13: Number(rateio13.replace(regdot,'').replace(',','.')),
+                                                    rateio12: Number(rateio12.replace(regdot,'').replace(',','.')),
+                                                    rateio11: Number(rateio11.replace(regdot,'').replace(',','.')),
+                                                    acumulado15: Number(acumulado15.replace(regdot,'').replace(',','.')),
+                                                    estimativa: Number(estimativa.replace(regdot,'').replace(',','.')),
+                                                    acumuladorEspecial: Number(acumuladorEspecial.replace(regdot,'').replace(',','.'))
                                                 };
                                                 jogo.cidades.push(cidade);
                                                 jogo.ufs.push(uf);
@@ -129,12 +147,13 @@ loto.controller('FacilCtrl', ['$scope', function($scope){
                                         } else {
                                             if(atual !== 0) {
                                                 var ufAtual = $(this).find('td:eq(1)').html();
-                                                jogos.lista[(jogos.lista.length - 1)].cidades.push(concurso);
-                                                jogos.lista[(jogos.lista.length - 1)].ufs.push(ufAtual);
+                                                jogos.lista[(jogos.lista.length - 1)].cidades.push(concurso.trim());
+                                                jogos.lista[(jogos.lista.length - 1)].ufs.push(ufAtual.trim());
                                             }
                                         }
                                     });
                                     scope.jogos = jogos.lista;
+                                    console.log(JSON.stringify(jogos));
                                     waitingDialog.hide();
                                 }
                             }
